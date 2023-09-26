e9e9a9139b7dcf3345b44c5720ec2be42ab92fd2
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2023-09-26 18:29:18 +0300

    Added props+emits features in site, feature: you can add contact

--- unit7/vue-first-app1/components/FriendContact.vue
+++ unit7/vue-first-app1/components/FriendContact.vue
@@ -1,34 +1,72 @@
 <template>
     <li>
-        <h2>{{ friend.name }}</h2>
+        <h2>{{ name }} {{ isFavorite ? '(Favorite)' : '' }} </h2>
+        <button @click="toggleFavorite">
+            Toggle Favorite
+        </button>
         <button @click="toggleDetails">
             {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
         </button>
 
         <ul v-if="detailsAreVisible">
-            <li><strong>Phone:</strong> {{ friend.phone }}</li>
-            <li><strong>Email:</strong> {{ friend.email }}</li>
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailAddress }}</li>
         </ul>
     </li>
 </template>
 
 <script>
 export default {
+    // props: [
+    //     'name',
+    //     'phoneNumber',
+    //     'emailAddress',
+    // ],
+    props: {
+        id: {
+            type: String,
+            requiered: true,
+        },
+        name: {
+            type: String,
+            requiered: true,
+        },
+        phoneNumber: {
+            type: String,
+            requiered: true,
+        },
+        emailAddress: {
+            type: String,
+            requiered: true,
+        },
+        isFavorite: {
+            type: Boolean,
+            requiered: false,
+            default: false,
+        },
+    },
+    // emits: ['toggle-favorite'],
+    emits: {
+        'toggle-favorite': function(id) {
+            if (id)
+                return true;
+            else{
+                console.warn('Id is missing');
+                return false;
+            }
+        }
+    },
     data() {
         return {
             detailsAreVisible: false,
-            friend:
-            {
-                id: "manuel",
-                name: "Manuel Lorenz",
-                phone: "01234 5678 991",
-                email: "manuel@localhost.com",
-            }
         }
     },
     methods: {
         toggleDetails() {
             this.detailsAreVisible = !this.detailsAreVisible;
+        },
+        toggleFavorite() {
+            this.$emit('toggle-favorite', this.id);
         }
     },
 }
--- unit7/vue-first-app1/src/App.vue
+++ unit7/vue-first-app1/src/App.vue
@@ -3,9 +3,19 @@
         <header>
             <h1>My friends</h1>
         </header>
+        <new-friend @add-contact="addContact"></new-friend>
         <ul>
-            <friend-contact></friend-contact>
-            <friend-contact></friend-contact>
+            <friend-contact 
+                v-for="friend in friends" 
+                :key="friend.id"
+                :id="friend.id"
+                :name="friend.name" 
+                :phone-number="friend.phone"
+                :email-address="friend.email"
+                :is-favorite="friend.isFavorite"
+                @toggle-favorite="toggleFavoriteStatus">
+            </friend-contact>
+            <!-- string olmayan bir js değeri gireceksen v-bind'a çevir prop'u -->
         </ul>
     </section>
 </template>
@@ -20,16 +30,34 @@ export default {
                     name: "Manuel Lorenz",
                     phone: "01234 5678 991",
                     email: "manuel@localhost.com",
+                    isFavorite: true,
                 },
                 {
                     id: "julie",
                     name: "Juile Jones",
                     phone: "09876 543 221",
                     email: "julie@localhost.com",
+                    isFavorite: false,
                 },
             ],
         }
-    }
+    },
+    methods: {
+        toggleFavoriteStatus(friendId) {
+            const selectedFriend = this.friends.find(friend => friend.id === friendId);
+            selectedFriend.isFavorite = !selectedFriend.isFavorite;
+        },
+        addContact(name, phone, email){
+            const newFriendContact = {
+                id: new Date().toISOString(),
+                name: name,
+                phone: phone,
+                email: email,
+                isFavorite: false,
+            };
+            this.friends.push(newFriendContact);
+        }
+    },
 }
 </script>
 
@@ -66,7 +94,8 @@ header {
     list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
     margin: 1rem auto;
     border-radius: 10px;
@@ -98,5 +127,18 @@ header {
     background-color: #ec3169;
     border-color: #ec3169;
     box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
