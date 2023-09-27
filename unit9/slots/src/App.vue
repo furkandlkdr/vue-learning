e8f77280c3e4f5562aef4812353e4f095ae25b53
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2023-09-27 14:59:49 +0300

    Added unit9's folder. We used local component instead of global

---
+++ unit9/slots/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <the-header></the-header>
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"
+    ></user-info>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue'
+import UserInfo from './components/UserInfo.vue';
+import BadgeList from './components/BadgeList.vue';
+
+export default {
+  components: {
+    UserInfo: UserInfo, //you can use like this vue will understand
+    //'the-header': TheHeader,
+    TheHeader,
+    BadgeList,
+  },
+  data() {
+    return {
+      activeUser: {
+        name: 'Maximilian Schwarzmüller',
+        description: 'Site owner and admin',
+        role: 'admin',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
