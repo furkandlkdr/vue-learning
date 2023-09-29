012d1eceff3f452f9d00315ed819614346bad97c
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2023-09-29 10:39:30 +0300

    Added form use of Vue

--- unit11/src/components/TheForm.vue
+++ unit11/src/components/TheForm.vue
@@ -1,16 +1,17 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{invalid: userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="formData.userName"/>
+      <input id="user-name" name="user-name" type="text" v-model.trim="formData.userName" @blur="validateInput"/>
+      <p v-if="userNameValidity === 'invalid'">Please enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model="formData.age" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="formData.referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,32 +20,39 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input id="interest-news" name="interest" type="checkbox" value="new" v-model="formData.interest" />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="formData.interest" />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="formData.interest" />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input id="how-video" name="how" type="radio" value="video" v-model="formData.how" />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="formData.how" />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input id="how-other" name="how" type="radio" value="other" v-model="formData.how" />
         <label for="how-other">Other</label>
       </div>
+    </div>
+    <div class="form-control">
+      <rating-control v-model="formData.rating"></rating-control>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="formData.confirm">
+      <label for="confirm-terms">Agree to terms of use?</label>
     </div>
     <div>
       <button>Save Data</button>
@@ -53,7 +61,12 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue';
+
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       formData: {
@@ -61,14 +74,31 @@ export default {
         age: null,
         referrer: 'google',
         interest: [],
-        how: ''
-      }
+        how: '',
+        confirm: false,
+        rating: null,
+      },
+      userNameValidity: 'pending',
     };
   },
   methods: {
     submitForm() {
       this.formData.userName = '';
+      this.formData.age = null;
+      this.formData.referrer = 'google';
+      this.formData.interest = [];
+      this.formData.how = '';
+      this.formData.confirm = false;
+      this.formData.rating = null;
+
       alert('Submitted!');
+    },
+    validateInput() {
+      if (this.formData.userName.length < 4) {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
     }
   }
 }
@@ -86,6 +116,12 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+.form-control.invalid label {
+  color: red;
+}
+.form-control.invalid input {
+  border-color: red;
 }
 
 label {
@@ -116,8 +152,8 @@ input[type='radio'] {
   margin-right: 1rem;
 }
 
-input[type='checkbox'] + label,
-input[type='radio'] + label {
+input[type='checkbox']+label,
+input[type='radio']+label {
   font-weight: normal;
 }
 
